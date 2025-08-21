a408c560335172b960fd8e2aa25a0912f2cc96ed
SL-DINU-BOY <139786264+SL-DINU-BOY@users.noreply.github.com>
2025-08-21 17:06:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || ''
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~FIghWaDB#QamrDBlr7iKyOSMCt0zsOQ-1ft_3HhlU9wfQUxV0hMU'
 };
